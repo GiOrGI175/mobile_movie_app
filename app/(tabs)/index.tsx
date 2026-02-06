ed54700c541de50d37ce2681151e4d190ec092d2
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2026-02-06 20:51:16 +0400

    ს

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,10 @@
-import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
-import './global.css';
 
 export default function Index() {
   return (
     <View className='flex-1 items-center justify-center bg-white'>
       <Text className='font-bold text-primary text-xl '>
         Welcome to Nativewind ssss!
-        <Link href='./onboarding'>Onboarding</Link>
       </Text>
     </View>
   );
